da797a86a17cd78d9f9b2f7e23d68b12d7ad2e08
Jek-San <ihsanzack@gmail.com>
2022-11-09 05:12:09 +0700

    done

--- pages/component/Header.js
+++ pages/component/Header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import cv from "../assets/resume.ico"
+
 export default function Header() {
   return <div className="fixed flex item-center justify-center w-full pt-8  text-white">
     <div className="bg-[#22222e] flex items-center justify-center  space-x-3 h-16 w-[20rem] rounded-[1.5rem] border border-gray-500">
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         
       </Head>
-      <div className=''>
+      <div className='overflow-hidden'>
         <Header />
         <Hero/>
     </div>     
